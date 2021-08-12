4c5122074d5b8b6a19095e40230841ce29e3e4e6
Davide Corsi <davide.corsi@par-tec.it>
2021-08-12 10:58:46 +0200

    first commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+/*
+ In questo esercizio bisogna leggere il file stream_reader.txt. Il contenuto deve essere trasformato in maiuscolo
+ e scritto su un nuovo file. Il file di lettura e scrittura vanno passati da terminale.
+ */
+public class Main {
+	public static void main(String[] args) {
+		
+	}
+}
